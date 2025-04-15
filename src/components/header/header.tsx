49a6d36f20a84ba30a378507fb69f54442649f97
Michael <171223687+Bazorko@users.noreply.github.com>
2025-04-15 02:15:17 -0400

    - Reformatted Header UI to be more responsive on smaller viewports.

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -23,9 +23,9 @@ const Header = () => {
                 <h1 className="text-2xl font-medium">Caelum</h1>
             </div>
             <div className="grow content-center">
-                <form onSubmit={handleSubmit} className="flex gap-0.5">
-                    <input type="text" value={inputText} onChange={(event) => setInputText(event.target.value)} placeholder="Enter location name" className="py-2 pl-2 text-neutral-900 border-2 border-r-1 rounded-l-lg bg-neutral-50 border-neutral-300" />
-                    <button className="px-2 border-2 border-l-1 rounded-r-lg bg-neutral-50 border-neutral-300">
+                <form onSubmit={handleSubmit} className="flex flex-col md:flex-row gap-1">
+                    <input type="text" value={inputText} onChange={(event) => setInputText(event.target.value)} placeholder="Enter location name" className="py-2 pl-2 text-neutral-900 border-2 border-r-1 rounded-lg md:rounded-none md:rounded-l-lg bg-neutral-50 border-neutral-300" />
+                    <button className="flex justify-center w-full py-2 md:px-2 border-2 border-l-1 rounded-lg md:rounded-none md:rounded-r-lg bg-neutral-50 border-neutral-300">
                         <SearchIcon />
                     </button>
                 </form>
--- src/components/weather/weather.tsx
+++ src/components/weather/weather.tsx
@@ -34,7 +34,7 @@ const Weather = () => {
         return(
             <section className="flex flex-col grow ">
                 <div className="grow justify-items-center content-center">
-                    <p>Test</p>
+                    <p>Find the weather data for anywhere in the U.S</p>
                 </div>
                 <div className="grow">
 
@@ -45,31 +45,3 @@ const Weather = () => {
 
 }
 export default Weather;
-
-/*
-
-
-return(
-        <>
-            <section className="flex w-full">
-            { weather ? (
-                <>
-                    <h2>{ locale.city }</h2>
-                    <h3>{ locale.state }</h3>
-
-                    <p>{ weather.weather[0].main }</p>
-                    <p>{ weather.weather[0].description }</p>
-
-                    <h5>{ weather.main.temp }</h5>
-                    <p>{ weather.main.temp_min }</p>
-                    <p>{ weather.main.temp_max }</p>
-                </>
-            ) : (
-                <p>Wondering what's the weather?</p>
-            ) }
-            </section>
-        </>
-    );
-
-
-*/
